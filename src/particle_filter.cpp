892f078a9941854de6a0e068e9ca442e8d9d4c41
Chris Ameter <cameter@gmail.com>
2018-03-20 17:15:11 -0600

    tweaked particle filter settings.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -24,7 +24,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
   
-  num_particles = 50;
+  num_particles = 100;
   
   default_random_engine gen;
   
